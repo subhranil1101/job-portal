aefcb5dcdd23475fc9d0c9fdd9fc1df7d6c2d55c
Subhranil Das <subhra750nil@gmail.com>
2024-09-21 00:57:59 +0530

    upto db model

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from 'dotenv'
+import connectDB from './utils/db.js'
+
+dotenv.config({})
+
+
+const app = express();
+
+
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
+const corsOptions = {
+      origin: 'http://localhost:5173',
+      credentials: true
+}
+app.use(cors(corsOptions))
+const PORT = 3000;
+
+app.listen(PORT, async () => {
+      await connectDB()
+      console.log(`Server running at ` + `http://localhost:${PORT}`)
+})
