51d15813c769bc397dad912a73b73970bf15a400
Subhranil Das <subhra750nil@gmail.com>
2024-10-03 01:06:33 +0530

    carousel edit

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -6,17 +6,21 @@ const category = [
       "Backend Developer",
       "Graphic Designer",
       "Fullstack Developer",
+      "Java Developer",
       "Data Scientist"
 ]
 const CategoryCarousel = () => {
       return (
             <div>
-                  <Carousel className="mx-auto w-full max-w-xl mt-16 mb-10" >
+                  <Carousel className="mx-auto w-full max-w-xl mt-16 mb-10" opts={{
+                        align: "start",
+                        loop: true,
+                  }} >
                         <CarouselContent className="-ml-2">
                               {category.map((cat, index) => {
                                     return (
-                                          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 pl-3 ">
-                                                <Button variant="outline" className="hover:bg-gray-300 font-mono font-semibold rounded-full">{cat}</Button>
+                                          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 flex justify-evenly items-center ">
+                                                <Button variant="outline" className="hover:bg-gray-100 font-mono font-semibold rounded-full">{cat}</Button>
                                           </CarouselItem>
                                     )
                               })}
