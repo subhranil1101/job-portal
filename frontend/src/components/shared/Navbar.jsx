f5798d0d4c1ed5d167ad761da946927c1cda2817
Subhranil Das <subhra750nil@gmail.com>
2024-10-11 13:18:53 +0530

    logout done

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -2,11 +2,32 @@ import { LogOut, User2 } from "lucide-react";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import axios from "axios";
+import { USER_API_ENDPOINT } from "@/utils/constant";
+import { setUser } from "@/redux/authSlice";
 
 const Navbar = () => {
+
       const { user } = useSelector(store => store.auth)
+      const dispatch = useDispatch()
+      const navigate = useNavigate()
+      const logoutHandler = async () => {
+            try {
+                  const res = await axios.get(`${USER_API_ENDPOINT}/logout`, { withCredentials: true })
+                  if (res.data.success) {
+                        dispatch(setUser(null))
+                        navigate('/')
+                        toast.success(res.data.message)
+                  }
+            } catch (error) {
+                  console.log(error)
+                  toast.error(error.response.data.message)
+            }
+      }
+
       return (
             <div className="bg-white flex justify-between my-2 items-center mx-auto max-w-[90%] h-16">
                   <Link to='/'><div className="text-3xl font-bold">
@@ -46,7 +67,7 @@ const Navbar = () => {
                                           </div>
                                           <div className="w-[90%] flex mt-5 justify-between text-sm">
                                                 <Link to='/profile'><Button variant='link'> <User2 />&nbsp;View Profile</Button></Link>
-                                                <Button variant='destructive' className='bg-red-600 rounded-2xl hover:bg-red-500 text-white'><LogOut />&nbsp;Logout</Button>
+                                                <Button onClick={logoutHandler} variant='destructive' className='bg-red-600 rounded-2xl hover:bg-red-500 text-white'><LogOut />&nbsp;Logout</Button>
 
                                           </div>
                                     </PopoverContent>
