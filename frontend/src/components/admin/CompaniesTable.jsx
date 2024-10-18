2228de92f28f6b59af7bb878538de7631fd98950
Subhranil Das <subhra750nil@gmail.com>
2024-10-19 00:24:45 +0530

    companies page ui update

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,10 +1,12 @@
-import { EditIcon, MoreHorizontalIcon, TrashIcon } from "lucide-react"
+import { EditIcon, MoreHorizontalIcon, SquareArrowOutUpRight, TrashIcon } from "lucide-react"
 import { Avatar, AvatarImage } from "../ui/avatar"
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
 import { useSelector } from "react-redux"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { motion } from "framer-motion"
+
 
 const CompaniesTable = () => {
       const navigate = useNavigate()
@@ -21,54 +23,82 @@ const CompaniesTable = () => {
             setFilterCompany(filteredCompany)
       }, [companies, searchCompanyByName])
       return (
-            <div>
-                  <Table>
-                        <TableCaption>A List of Your Registered Companies</TableCaption>
-                        <TableHeader>
-                              <TableRow>
-                                    <TableHead>Logo</TableHead>
-                                    <TableHead>Name</TableHead>
-                                    <TableHead>Description</TableHead>
-                                    <TableHead>Website</TableHead>
-                                    <TableHead>Location</TableHead>
-                                    <TableHead>Date</TableHead>
-                                    <TableHead className='text-right'>Action</TableHead>
-                              </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                              {filterCompany?.map((company) => (
-                                    <tr key={company._id}>
-                                          <TableCell>
-                                                <Avatar>
-                                                      <AvatarImage src={company?.logo} />
-                                                </Avatar>
-                                          </TableCell>
-                                          <TableCell className="text-xl font-bold">{company?.name || "Add company Name"}</TableCell>
-                                          <TableCell className="text-base">{company?.description || "Add company description"}</TableCell>
-                                          <TableCell className="text-sm">
+            <div className="border border-black p-5 rounded-3xl shadow-xl">
+                  {
+                        !filterCompany ?
+                              <p className="text-red-500 font-bold text-center text-xl">
+                                    *No Company to display. Please register your 1st Company
+                              </p>
+                              :
+                              <Table>
+                                    <TableCaption className="text-blue-900 font-medium text-base italic">A List of Your Registered Companies</TableCaption>
+                                    <TableHeader>
+                                          <TableRow className="text-xl font-serif shadow shadow-slate-700">
+                                                <TableHead>Logo</TableHead>
+                                                <TableHead>Name</TableHead>
+                                                <TableHead>Description</TableHead>
+                                                {/* <TableHead>Website</TableHead> */}
+                                                <TableHead>Location</TableHead>
+                                                <TableHead>Date</TableHead>
+                                                <TableHead className='text-right'>Action</TableHead>
+                                          </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                          {filterCompany?.map((company) => (
+                                                <motion.tr
+                                                      initial={{ opacity: 0, x: -100 }}
+                                                      animate={{ opacity: 1, x: 0 }}
+                                                      exit={{ opacity: 0, x: 100 }}
+                                                      transition={{ duration: 0.5 }}
+                                                      key={company._id}>
+                                                      <TableCell className='drop-shadow-xl'>
+                                                            <Avatar>
+                                                                  <AvatarImage src={company?.logo} />
+                                                            </Avatar>
+                                                      </TableCell>
+                                                      <TableCell
+                                                            className="text-xl font-bold flex gap-2 items-center justify-start hover:scale-105 cursor-default"
+                                                      >
+                                                            {company?.name}
+
+                                                            {
+                                                                  company?.website && <a
+                                                                        href={company?.website}
+                                                                        target="_blank"
+                                                                        rel="noopener noreferrer"
+                                                                        className="text-gray-500 hover:text-gray-900"
+                                                                  >
+                                                                        <SquareArrowOutUpRight />
+                                                                  </a>
+                                                            }
+                                                      </TableCell>
+                                                      <TableCell className="text-base font-medium">{company?.description || <span className="text-gray-500 text-xs cursor-pointer hover:text-gray-700" onClick={() => navigate(`/admin/companies/${company._id}`)}>Add Company Description</span>}</TableCell>
+                                                      {/* <TableCell className="text-sm">
                                                 <a className="hover:text-blue-700 hover:underline" href={company?.website} target="_blank" rel="noopener noreferrer" >{company?.website || "Add company website"}</a>
-                                          </TableCell>
-                                          <TableCell>{company?.location || "Add company location"}</TableCell>
-                                          <TableCell>{company?.createdAt.split("T")[0]}</TableCell>
-                                          <TableCell className='text-right'>
-                                                <Popover>
-                                                      <PopoverTrigger><MoreHorizontalIcon /></PopoverTrigger>
-                                                      <PopoverContent className='rounded-3xl shadow-xl w-fit bg-slate-50 flex flex-col gap-5 justify-start items-start'>
-                                                            <div onClick={() => navigate(`/admin/companies/${company._id}`)} className="flex gap-2 items-center cursor-pointer">
-                                                                  <EditIcon size='20' />
-                                                                  <span>Edit</span>
-                                                            </div>
-                                                            <div className="flex gap-2 items-center cursor-pointer">
-                                                                  <TrashIcon size='20' />
-                                                                  <span>Delete</span>
-                                                            </div>
-                                                      </PopoverContent>
-                                                </Popover>
-                                          </TableCell>
-                                    </tr>
-                              ))}
-                        </TableBody>
-                  </Table>
+                                          </TableCell> */}
+                                                      <TableCell className="text-base font-medium">{company?.location || <span className="text-gray-500 text-xs cursor-pointer hover:text-gray-700" onClick={() => navigate(`/admin/companies/${company._id}`)}>Add Company Location</span>}</TableCell>
+
+                                                      <TableCell>{company?.createdAt.split("T")[0]}</TableCell>
+                                                      <TableCell className='text-right'>
+                                                            <Popover>
+                                                                  <PopoverTrigger><MoreHorizontalIcon /></PopoverTrigger>
+                                                                  <PopoverContent className='rounded-3xl shadow-xl w-fit bg-slate-50 flex flex-col gap-5 justify-start items-start'>
+                                                                        <div onClick={() => navigate(`/admin/companies/${company._id}`)} className="flex gap-2 items-center cursor-pointer">
+                                                                              <EditIcon size='20' />
+                                                                              <span>Edit</span>
+                                                                        </div>
+                                                                        <div className="flex gap-2 items-center cursor-pointer">
+                                                                              <TrashIcon size='20' />
+                                                                              <span>Delete</span>
+                                                                        </div>
+                                                                  </PopoverContent>
+                                                            </Popover>
+                                                      </TableCell>
+                                                </motion.tr>
+                                          ))}
+                                    </TableBody>
+                              </Table>
+                  }
             </div>
       )
 }
