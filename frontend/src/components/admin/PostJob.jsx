86b84b005062cc39d52af3765f552c73e7168755
Subhranil Das <subhra750nil@gmail.com>
2024-10-19 01:30:07 +0530

    post job ui update

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -8,6 +8,7 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 import axios from "axios"
 import { JOB_API_ENDPOINT } from "@/utils/constant"
 import { toast } from "sonner"
+import { Avatar, AvatarImage } from "../ui/avatar"
 
 const PostJob = () => {
       const navigate = useNavigate()
@@ -62,52 +63,66 @@ const PostJob = () => {
                   <form onSubmit={submitHandler} className="border border-gray-600 shadow-2xl w-fit mx-auto rounded-2xl my-5 flex flex-col items-center justify-center">
                         <div className="flex items-center justify-center max-w-7xl mx-auto my-5 flex-wrap gap-7">
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Job Title</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Job Title*</label>
                                     <input placeholder="Enter Job Title" type="text" name="title" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.title} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Job Description</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Job Description*</label>
                                     <input placeholder="Enter Job Description" type="text" name="description" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.description} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Job Requirements</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Job Requirements*</label>
                                     <input placeholder="Enter Job Requirements" type="text" name="requirements" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.requirements} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Salary</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Salary*</label>
                                     <input placeholder="Enter Salary in LPA" type="number" name="salary" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.salary} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Location</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Location*</label>
                                     <input placeholder="Enter Job Location" type="text" name="location" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.location} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Job Type</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Job Type*</label>
                                     <input placeholder="Enter Fulltime or Internship" type="text" name="jobType" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.jobType} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Experience</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Experience*</label>
                                     <input placeholder="Enter Experience Level" type="number" name="experience" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.experience} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
-                                    <label className="px-2 py-2 text-xl font-medium">Positions</label>
+                                    <label className="px-2 py-2 text-xl font-medium">Positions*</label>
                                     <input placeholder="Enter number of Vacancies" type="number" name="position" className="text-xl px-2 py-1 rounded-full outline-none border border-black" value={input.position} onChange={changeEventHandler} />
                               </div>
                               <div className="w-[45%] flex flex-col">
                                     {
                                           companies.length > 0 && (
                                                 <Select onValueChange={selectChangeHandler}>
-                                                      <SelectTrigger className="rounded-full w-1/2 mx-auto mt-5 text-xl font-medium">
+                                                      <SelectTrigger className="rounded-full w-1/2 mx-auto mt-5 py-5 text-xl font-medium">
                                                             <SelectValue placeholder="Select a Company" />
                                                       </SelectTrigger>
-                                                      <SelectContent className="bg-white shadow-xl text-justify text-xl">
+                                                      <SelectContent className="bg-white shadow-xl text-justify text-xl overflow-hidden">
                                                             <SelectGroup>
                                                                   {
-                                                                        companies.map((company) => {
-                                                                              return (
-                                                                                    <SelectItem value={company?.name?.toLowerCase()} key={company._id}>{company?.name}</SelectItem>
-                                                                              )
-                                                                        })
+                                                                        companies
+                                                                              .filter(company => company?.website && company?.logo)
+                                                                              .map((company) => {
+                                                                                    return (
+                                                                                          <SelectItem
+                                                                                                value={company?.name?.toLowerCase()}
+                                                                                                key={company._id}
+                                                                                                className="border border-black mx-auto mt-1 p-2 bg-slate-100 hover:bg-slate-50 cursor-pointer rounded-3xl "
+                                                                                          >
+                                                                                                <div className="flex items-center gap-5 font-serif text-base">
+                                                                                                      <Avatar className="drop-shadow-xl">
+                                                                                                            <AvatarImage src={company?.logo} />
+                                                                                                      </Avatar>
+                                                                                                      {company?.name}
+                                                                                                </div>
+                                                                                                {/* {company?.name} */}
+                                                                                          </SelectItem>
+                                                                                    )
+                                                                              })
                                                                   }
                                                             </SelectGroup>
                                                       </SelectContent>
@@ -118,10 +133,11 @@ const PostJob = () => {
                         </div>
                         <div>
                               {
-                                    loading ? <Button className="cursor-not-allowed my-5 flex items-center justify-center border border-black rounded-full bg-green-500 hover:bg-green-600 font-bold"><Loader2 className="mr-2 h-5 w-5 animate-spin" />&nbsp;Please wait...</Button> :
+                                    loading ? <Button className="cursor-not-allowed my-5 flex items-center justify-center border border-black rounded-full bg-green-500 hover:bg-green-600 font-bold"><Loader2 className="mr-2 h-5 w-5 animate-spin" />&nbsp;Please wait...</Button>
+                                          :
                                           <div className="flex gap-10 my-5">
                                                 <Button onClick={() => navigate("/admin/jobs")} className="flex items-center justify-center font-bold border border-black rounded-full hover:bg-gray-200"><ArrowLeft size={20} />&nbsp;Cancel</Button>
-                                                <Button type="submit" disabled={companies.length <= 0} className="flex items-center justify-center border border-black rounded-full bg-green-500 hover:bg-green-600 font-bold"><Save />&nbsp;Submit</Button>
+                                                <Button type="submit" disabled={!input.companyId} className="flex items-center justify-center border border-black rounded-full bg-green-500 hover:bg-green-600 font-bold"><Save />&nbsp;Submit</Button>
                                           </div>
                               }
                         </div>
