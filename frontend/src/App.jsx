16e8d3f4cf90e3e4f431453134f298c3bca15054
Subhranil Das <subhra750nil@gmail.com>
2024-10-19 19:49:45 +0530

    login signup navigation update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
@@ -13,69 +14,81 @@ import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from "./components/admin/PostJob";
 import Applicants from "./components/admin/Applicants";
 import ProtectedRoute from "./components/admin/ProtectedRoute";
-import { useSelector } from "react-redux";
 
 function App() {
+  // Get user info from Redux store
   const { user } = useSelector((store) => store.auth);
 
-  const appRouter = createBrowserRouter([
-    {
-      path: '/',
-      element: !user ? <Home /> : user?.role === 'student' ? <Home /> : <ProtectedRoute><Companies /></ProtectedRoute>
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/signup',
-      element: <Signup />
-    },
-    {
-      path: '/jobs',
-      element: <Jobs />
-    },
-    {
-      path: '/jobs/description/:id',
-      element: <JobDescription />
-    },
-    {
-      path: '/browse',
-      element: <Browse />
-    },
-    {
-      path: '/profile',
-      element: <Profile />
-    },
-    // admin routes
-    {
-      path: '/admin/companies',
-      element: <ProtectedRoute><Companies /></ProtectedRoute>
-    },
-    {
-      path: '/admin/companies/create',
-      element: <ProtectedRoute><CompanyCreate /></ProtectedRoute>
-    },
-    {
-      path: '/admin/companies/:id',
-      element: <ProtectedRoute><CompanySetup /></ProtectedRoute>
-    },
-    {
-      path: '/admin/jobs',
-      element: <ProtectedRoute><AdminJobs /></ProtectedRoute>
-    },
-    {
-      path: '/admin/jobs/create',
-      element: <ProtectedRoute><PostJob /></ProtectedRoute>
-    },
-    {
-      path: '/admin/jobs/:id/applicants',
-      element: <ProtectedRoute><Applicants /></ProtectedRoute>
-    }
-  ]);
-
   return (
-    <RouterProvider router={appRouter} />
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route
+          path="/"
+          element={user ? (
+            user.role === "student" ? <Home /> : <Navigate to="/admin/companies" />
+          ) : (
+            <Home />
+          )}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/jobs/description/:id" element={<JobDescription />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/profile" element={<Profile />} />
+
+        {/* Protected routes for admin (recruiter) */}
+        <Route
+          path="/admin/companies"
+          element={
+            <ProtectedRoute user={user} role="recruiter">
+              <Companies />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/companies/create"
+          element={
+            <ProtectedRoute user={user} role="recruiter">
+              <CompanyCreate />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/companies/:id"
+          element={
+            <ProtectedRoute user={user} role="recruiter">
+              <CompanySetup />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/jobs"
+          element={
+            <ProtectedRoute user={user} role="recruiter">
+              <AdminJobs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/jobs/create"
+          element={
+            <ProtectedRoute user={user} role="recruiter">
+              <PostJob />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/jobs/:id/applicants"
+          element={
+            <ProtectedRoute user={user} role="recruiter">
+              <Applicants />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/admin/ProtectedRoute.jsx
+++ frontend/src/components/admin/ProtectedRoute.jsx
@@ -1,23 +1,38 @@
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
+// import { useEffect } from "react"
+// import { useSelector } from "react-redux"
+// import { useNavigate } from "react-router-dom"
 
 
-const ProtectedRoute = ({ children }) => {
-      const { user } = useSelector(store => store.auth)
-      const navigate = useNavigate()
+// const ProtectedRoute = ({ children }) => {
+//       const { user } = useSelector(store => store.auth)
+//       const navigate = useNavigate()
 
-      useEffect(() => {
-            if (user === null || user.role !== 'recruiter') {
-                  navigate("/")
-            }
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [])
-      return (
-            <>
-                  {children}
-            </>
-      )
-}
+//       useEffect(() => {
+//             if (user === null || user.role !== 'recruiter') {
+//                   navigate("/")
+//             }
+//             // eslint-disable-next-line react-hooks/exhaustive-deps
+//       }, [])
+//       return (
+//             <>
+//                   {children}
+//             </>
+//       )
+// }
 
-export default ProtectedRoute
+// export default ProtectedRoute
+
+import { Navigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+const ProtectedRoute = ({ children, user, role }) => {
+      // Check if user is authenticated and has the required role
+      if (!user || user.role !== role) {
+            return <Navigate to="/login" />;
+      }
+
+      // Render the child components (admin pages)
+      return children;
+};
+
+export default ProtectedRoute;
