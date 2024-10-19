cb34261be0f8e43fe03e3762024c5743c97d5a5f
Subhranil Das <subhra750nil@gmail.com>
2024-10-19 22:42:00 +0530

    footer ui update

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
       return (
-            <div className="w-full text-center mt-10 text-base bg-gray-400 ">
-                  <div className="py-1 text-xl font-bold">Job<span className="text-blue-900">Cat</span></div>
-                  <div className="font-mono" >Copyright &copy; {new Date().getFullYear()} | All Rights Reserved Subhranil Das </div>
+            <div className="w-full text-center mt-10 text-base bg-gray-400  flex flex-col justify-center items-center ">
+                  <span className="py-1 text-xl font-bold font-sans">Job<span className="text-blue-900">Cat</span></span>
+                  <span className="font-mono" >Copyright &copy; {new Date().getFullYear()} | All Rights Reserved Subhranil Das </span>
             </div>
       )
 }
